a2ebb9d6384264b687687891b52c883190ee5383
MagicalAlexey <animif220@gmail.com>
2022-09-20 22:04:31 +0300

    Revert "Temporal remove Crystallized Dimension"
    This reverts commit 01fba56dac87e41a5b15dfc87698241d148d555d.

--- src/main/java/power/powerdimensions/init/PowerDimensionsModBiomes.java
+++ src/main/java/power/powerdimensions/init/PowerDimensionsModBiomes.java
@@ -8,6 +8,8 @@ import power.powerdimensions.world.biome.SunPlainsBiome;
 import power.powerdimensions.world.biome.OblivionPlainsBiome;
 import power.powerdimensions.world.biome.OblivionForestBiome;
 import power.powerdimensions.world.biome.OblivionDesertBiome;
+import power.powerdimensions.world.biome.CrystallizedLandsBiome;
+import power.powerdimensions.world.biome.CrystallizedForestBiome;
 import power.powerdimensions.PowerDimensionsMod;
 
 import net.minecraftforge.registries.RegistryObject;
@@ -43,9 +45,13 @@ import com.mojang.datafixers.util.Pair;
 
 public class PowerDimensionsModBiomes {
 	public static final DeferredRegister<Biome> REGISTRY = DeferredRegister.create(ForgeRegistries.BIOMES, PowerDimensionsMod.MODID);
+	public static final RegistryObject<Biome> CRYSTALLIZED_LANDS = REGISTRY.register("crystallized_lands",
+			() -> CrystallizedLandsBiome.createBiome());
 	public static final RegistryObject<Biome> OBLIVION_FOREST = REGISTRY.register("oblivion_forest", () -> OblivionForestBiome.createBiome());
 	public static final RegistryObject<Biome> OBLIVION_PLAINS = REGISTRY.register("oblivion_plains", () -> OblivionPlainsBiome.createBiome());
 	public static final RegistryObject<Biome> OBLIVION_DESERT = REGISTRY.register("oblivion_desert", () -> OblivionDesertBiome.createBiome());
+	public static final RegistryObject<Biome> CRYSTALLIZED_FOREST = REGISTRY.register("crystallized_forest",
+			() -> CrystallizedForestBiome.createBiome());
 	public static final RegistryObject<Biome> SUN_PLAINS = REGISTRY.register("sun_plains", () -> SunPlainsBiome.createBiome());
 
 	@SubscribeEvent
--- src/main/java/power/powerdimensions/init/PowerDimensionsModItems.java
+++ src/main/java/power/powerdimensions/init/PowerDimensionsModItems.java
@@ -13,11 +13,31 @@ import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
 
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.BlockItem;
 
 public class PowerDimensionsModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, PowerDimensionsMod.MODID);
 	public static final RegistryObject<Item> CRYSTALLIZED = REGISTRY.register("crystallized", () -> new CrystallizedItem());
 	public static final RegistryObject<Item> OBLIVION = REGISTRY.register("oblivion", () -> new OblivionItem());
 	public static final RegistryObject<Item> SUNNY_KINGDOM = REGISTRY.register("sunny_kingdom", () -> new SunnyKingdomItem());
+	public static final RegistryObject<Item> CRYSTAL_BLUE = block(PowerDimensionsModBlocks.CRYSTAL_BLUE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_BROWN = block(PowerDimensionsModBlocks.CRYSTAL_BROWN, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_GRAY = block(PowerDimensionsModBlocks.CRYSTAL_GRAY, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_GREEN = block(PowerDimensionsModBlocks.CRYSTAL_GREEN, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_LIGHT_BLUE = block(PowerDimensionsModBlocks.CRYSTAL_LIGHT_BLUE,
+			CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_LIME = block(PowerDimensionsModBlocks.CRYSTAL_LIME, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_ORANGE = block(PowerDimensionsModBlocks.CRYSTAL_ORANGE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_PINK = block(PowerDimensionsModBlocks.CRYSTAL_PINK, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_PURPLE = block(PowerDimensionsModBlocks.CRYSTAL_PURPLE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_RED = block(PowerDimensionsModBlocks.CRYSTAL_RED, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_VIOLET = block(PowerDimensionsModBlocks.CRYSTAL_VIOLET, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CRYSTAL_YELLOW = block(PowerDimensionsModBlocks.CRYSTAL_YELLOW, CreativeModeTab.TAB_BUILDING_BLOCKS);
+
+	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
+		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+	}
 }
